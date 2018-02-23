a158ed989ac4df4a91eba9fa1f692a8fc7191f5e
Hack Reactor Student <student@hackreactor.com>
2018-02-22 19:03:34 -0500

    made render favorite function

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -32,6 +32,7 @@ class App extends React.Component {
     this.addFavorite = this.addFavorite.bind(this);
     this.onFavorite = this.onFavorite.bind(this);
     this.renderFavorite = this.renderFavorite.bind(this);
+    this.obtainFavorite = this.obtainFavorite.bind(this);
   }
 
   getAlcohols() {
@@ -138,6 +139,13 @@ class App extends React.Component {
     })
   }
 
+obtainFavorite(userFaves){
+    this.setState ({
+    drinks: JSON.parse(userFaves.drinks),
+    uriId: userFaves.music
+  })
+}
+
   componentDidMount() {
     this.renderFavorite();
   }
@@ -179,6 +187,7 @@ class App extends React.Component {
               favorited={this.state.favorited}
               renderFavorite={this.renderFavorite}
               listOfFavorites={this.state.listOfFavorites}
+              obtainFavorite={this.obtainFavorite}
             />
           </Grid.Row>
           <Grid.Row>
