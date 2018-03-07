2e35d1427735a1738481561c0bd98f5989b578f7
juangalan23 <juangalan.a.s@gmail.com>
2018-03-07 17:31:34 -0500

    has concert list with images

--- client/src/components/ConcertItem.jsx
+++ client/src/components/ConcertItem.jsx
@@ -11,13 +11,17 @@ class ConcertItem extends React.Component {
     render() {
         return(
             <div>
-                <Grid stackable> 
-                    <Grid.Row columns={16} centered>
-                        <Grid.Column width={5}>
-                            <img className="concertImage" src={this.props.concert.url} />
+                <Grid stackable className="concertGrid" > 
+                    <Grid.Row columns={16}>
+                        <Grid.Column width={3}>
+                            <img className="concertImage" src={this.props.concert.imageUrl} />
                         </Grid.Column>
-                        <Grid.Column width={11}>
-                            <p> random text </p>
+                        <Grid.Column width={13}>
+                            <div className="concertTextColumn">
+                                <p> <a className="concertName"  href={this.props.concert.url}  target="_blank"> {this.props.concert.name} </a></p>
+                                <p className="concertDescription"> {this.props.concert.venue} </p>
+                                <p className="concertDescription"> {this.props.concert.dateTime} </p>
+                            </div>
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
