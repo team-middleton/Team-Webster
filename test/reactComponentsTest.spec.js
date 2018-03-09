5c0c394e71589ad7584dd2badcd287bc1e4f959c
juangalan23 <juangalan.a.s@gmail.com>
2018-03-09 15:22:36 -0500

    somewhat made progress on test suite

---
+++ test/reactComponentsTest.spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import {expect} from 'chai';
+
+import ConcertsContainer from '../client/src/components/ConcertsContainer.jsx';
+import { wrap } from 'module';
+
+describe('< ConcertsContainer >', function() {
+    it('should exist', function() {
+
+        expect(wrapper).to.be.undefined;
+    })
+})
