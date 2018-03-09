91e07a8bcf7a19165cd94877814e117e246cd0f1
juangalan23 <juangalan.a.s@gmail.com>
2018-03-09 15:22:36 -0500

    somewhat made progress on test suite

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 var path = require('path');
+const webpack = require('webpack');
 var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
+  devServer: {
+    hot: true,
+    inline: true,
+    port: 7700,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: [ '.js', '.jsx'],
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/', 'react')
+    }
+  },
+  externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+  },
+  devtool: "source-map",
   output: {
     filename: 'bundle.js',
     path: DIST_DIR
@@ -15,14 +34,9 @@ module.exports = {
         include : SRC_DIR,
         loader : 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['env','airbnb', 'react', 'es2015', 'stage-3']
        }
      }
     ]
-  },
-  resolve: {
-    alias: {
-      'react': path.resolve(__dirname, './node_modules/', 'react')
-    }
   }
 };
