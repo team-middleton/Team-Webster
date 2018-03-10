36f32f86901aaa357835abe0c588408117b9e715
juangalan23 <juangalan.a.s@gmail.com>
2018-03-09 19:38:51 -0500

    fixed infinite set state loop in sign up, require valid email to create account

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -12,75 +12,104 @@ class Signup extends React.Component {
       password: '',
       email: '',
       disabled: true,
-      error: false
+      error: false,
+      messageHeader: '',
+      messageContent: ''
     }
     this.signup = this.signup.bind(this);
     this.onChangeUsername = this.onChangeUsername.bind(this);
     this.onChangePassword = this.onChangePassword.bind(this);
     this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.enableSignUp = this.enableSignUp.bind(this);
   }
 
   signup(e) {
-    e.preventDefault();
-    axios.post('/signup', {
-      username: this.state.username,
-      password: this.state.password,
-      email: this.state.email
-    }).then(() => {
-      this.setState({username: '', password: '', email: ''});
-      ReactDOM.render(<App />, document.getElementById('app'));
-    }).catch((error) => {
-      this.setState({
-        error: true
-      })
-      throw error;
+    // e.preventDefault();
+    axios.get('/emailCheck', {
+      params: {
+        email: this.state.email
+      }
+    })
+    .then((res)=> {
+      console.log('res in client ', res)
+      if( res.data === true) {
+        axios.post('/signup', {
+          username: this.state.username,
+          password: this.state.password,
+          email: this.state.email
+        }).then(() => {
+          this.setState({username: '', password: '', email: ''});
+          ReactDOM.render(<App />, document.getElementById('app'));
+        }).catch((error) => {
+          this.setState({
+            error: true
+          })
+          throw error;
+        })
+      } else {
+        this.setState({
+          messageHeader: 'Email not valid',
+          messageContent: 'Please try again with a valid email address'
+        })
+      }
+    })
+    .catch((err)=> {
+      console.log('err in client in verifying email ', err);
     })
   }
 
   onChangeUsername(e) {
+    e.preventDefault()
     this.setState({username: e.target.value})
   }
 
   onChangePassword(e) {
+    e.preventDefault()
     this.setState({password: e.target.value})
   }
 
   onChangeEmail(e) {
+    e.preventDefault()
     this.setState({email: e.target.value})
   }
 
+  enableSignUp() {
+    this.setState({
+      disabled: false
+    })
+  }
+
+
+ 
   render() {
-    let message;
     if (this.state.username.length === 0) {
-     message = <Message
-       warning
-       header='Enter a username'
-       content='Please make sure you enter a username.'
-     />;
+      var messageHeader='Enter a username'
+      var messageContent='Please make sure you enter a username.'
+      var button = <Button type='submit' disabled={true}>Sign Up</Button>
    }  else if (this.state.password.length < 8) {
-     message = <Message
-       warning
-       header='Enter valid password'
-       content='Please make sure you enter a valid password with at least 8 characters.'
-     />;
-   } else if (this.state.email.indexOf('@') === -1 || this.state.email.indexOf('.') === -1) {
-      message = <Message
-        warning
-        header='Enter valid E-mail Address'
-        content='Please make sure you enter a valid e-mail address.'
-      />;
-    } else {
-      this.setState({
-        disabled: false
-      })
+      var messageHeader='Enter valid password'
+      var messageContent='Please make sure you enter a valid password with at least 8 characters.'
+      var button = <Button type='submit' disabled={true}>Sign Up</Button>
+   } 
+   else if ( this.state.email.indexOf('.') === -1) {
+      var messageHeader='Enter valid E-mail Address'
+      var messageContent='Please make sure you enter a valid e-mail address.'
+      var button = <Button type='submit' disabled={true}>Sign Up</Button>
+    } 
+    else {
+      var messageHeader='Enter valid E-mail Address'
+      var messageContent='Please make sure you enter a valid e-mail address.'
+      var button = <Button type='submit' disabled={false}>Sign Up</Button>
     }
 
     if (this.state.error === true) {
-      message = <Message
-        error
-        header='We are sorry!'
-        content='Looks like we could not sign you up! We will look into it. In the meantime please try editing your credentials and try again.'
-      />;
+      var messageHeader='We are sorry!'
+      var messageContent='Looks like we could not sign you up! We will look into it. In the meantime please try editing your credentials and try again.'
+    }
+
+    if(this.state.messageHeader !== '') {
+      var messageHeader = this.state.messageHeader;
+      var messageContent = this.state.messageContent
     }
 
     return (
@@ -101,8 +130,13 @@ class Signup extends React.Component {
                 <label style={{color: 'white'}}>E-mail</label>
                 <input type='text' placeholder='Enter Email' value={this.state.email} onChange={this.onChangeEmail}/>
               </Form.Field>
-              {message}
-              <Button type='submit' disabled={this.state.disabled}>Sign Up</Button>
+
+              < Message 
+                warning
+                header={messageHeader}
+                content={messageContent}
+              /> 
+              {button}
             </Form>
           </Grid.Column>
         </Grid.Row>
