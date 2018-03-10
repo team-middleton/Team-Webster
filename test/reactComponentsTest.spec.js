bea7f27a47864c4658bb75cfaa77bd1a0d36fe7a
juangalan23 <juangalan.a.s@gmail.com>
2018-03-09 19:36:53 -0500

    fixed infinite set stating loop in sign up, and required valid email addresses in sign up

--- test/reactComponentsTest.spec.js
+++ test/reactComponentsTest.spec.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import {expect} from 'chai';
 
-import ConcertsContainer from '../client/src/components/ConcertsContainer.jsx';
-import { wrap } from 'module';
+import ConcertsContainer from '../../client/dist/components/ConcertsContainer.jsx';
+
+
 
 describe('< ConcertsContainer >', function() {
     it('should exist', function() {
-
+        const wrapper = shallow(< ConcertsContainer />);
         expect(wrapper).to.be.undefined;
     })
 })
