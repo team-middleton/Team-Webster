d9e57e335186b382bc68f0937d612ad0cb80d5b0
Marcus Hausler <marcus.hausler@gmail.com>
2018-03-10 15:35:38 -0500

    Style placeholder content

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -351,7 +351,7 @@ class App extends React.Component {
             </Grid.Column>
             <Grid.Column width={5}>
               <Grid.Row> 
-                  {rightSide}
+                {rightSide}
               </Grid.Row>
               <Grid.Row> 
                 {mapOrZipRequest}
